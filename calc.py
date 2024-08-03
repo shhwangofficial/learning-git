ac6f1b6022a8b10799bc52d52a2e09ace0a23c82
Sangha <shhwangofficial@kakao.com>
2024-08-03 16:36:25 +0900

    Add get_sum_ver2 function

--- calc.py
+++ calc.py
@@ -16,5 +16,8 @@ def divide_premium(a, b):
 def get_median(a, b):
     return (a+b)/2
 
-def get_sum_ver1(n):
-    return n(n+1)/2
+def getSum_ver2(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i   
+    return sum
